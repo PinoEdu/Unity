845c038d8efca84f578bfc087de7848572886208
Eduardo Pino <72419887+PinoEdu@users.noreply.github.com>
2023-06-07 19:59:51 -0400

    feat: control del vehiculo
    - Se agregaron las direccionales para moverse hacia adelante, atras, izquierda y derecha

--- Juego 1/Assets/Scripts/ControlJugador.cs
+++ Juego 1/Assets/Scripts/ControlJugador.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ControlJugador : MonoBehaviour
 {
-    public float Velocidad = 200.0f;
+    public float velocidad = 20.0f;
+    public float velocidadGiro = 5;
+    public float controlHorizontal;
+    public float controlAvance;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +18,11 @@ public class ControlJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        controlHorizontal = Input.GetAxis("Horizontal");
+        controlAvance = Input.GetAxis("Vertical");
         //Mover la furgoneta hacia delante
         // transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * Velocidad);
+        transform.Translate(Vector3.forward * Time.deltaTime * velocidad * controlAvance);
+        transform.Translate(Vector3.right * Time.deltaTime * velocidadGiro * controlHorizontal);
     }
 }
