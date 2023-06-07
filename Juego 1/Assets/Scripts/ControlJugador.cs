5987c2f9c78b500cf9b0d9f432b5450e38b1ecce
Eduardo Pino <72419887+PinoEdu@users.noreply.github.com>
2023-06-07 00:48:28 -0400

    feat: se agrego script de movimiento a la furgoneta

---
+++ Juego 1/Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mover la furgoneta hacia delante
+        // transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 15);
+    }
+}
